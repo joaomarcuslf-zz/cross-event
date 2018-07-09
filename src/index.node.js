ed55eb3fa2b11dbff3c2524b348498c35b5b4571
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2018-07-09 13:59:23 -0300

    Added Node Code

---
+++ src/index.node.js
@@ -0,0 +1,37 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+let events = [];
+
+const removeListener = ({ eventName, callback, element }) => {
+  element.removeListener(eventName, callback)
+};
+
+const CrossEvent = {
+  on: (eventName, callback = () => {}) => {
+    myEmitter.on(eventName, callback);
+
+    const listener = {
+      eventName,
+      callback,
+      element: myEmitter,
+    };
+
+    events.push(listener);
+
+    return listener;
+  },
+  rm: removeListener,
+  rmAll: () => {
+    events.map(removeListener);
+
+    events = [];
+  },
+  emit: (eventName, body = '') => {
+    return myEmitter.emit(eventName, body);
+  },
+}
+
+module.exports = CrossEvent;
+
